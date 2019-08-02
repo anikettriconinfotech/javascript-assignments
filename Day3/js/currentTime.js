66d1a88d9ddcad41623871fa47b20548f7a4e3c8
manoramtaparia <53037337+manoramtaparia@users.noreply.github.com>
2019-08-02 13:05:27 +0530

    Add files via upload

---
+++ Day3/js/currentTime.js
@@ -0,0 +1,17 @@
+var TimeBtn = document.querySelector('#TimeDisplay');
+TimeBtn.addEventListener('click', currentTime);
+
+function currentTime() {
+    var DateObj = new Date();
+    var hour = DateObj.getHours();
+    var min = DateObj.getMinutes();
+    var sec = DateObj.getSeconds();
+    var meridiem = 'AM';
+    if (hour > 12) {  
+        meridiem = 'PM';
+        hour = hour - 12;
+    }
+    var currTime = "Current time is " + hour + " : " + min + " : " + sec + " " + meridiem;
+    document.querySelector('#displayTime').innerHTML = currTime;
+    setTimeout(currentTime, 1000);
+}
